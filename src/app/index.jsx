5f56d7504b9964506abc2c88987a005c8458236d
Melle Wynia <contact@mellewynia.nl>
2017-03-20 13:44:07 +0100

    Add profile & New experience

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -5,6 +5,7 @@ import {render} from 'react-dom';
 import ExperienceWork from './ExperienceWork.jsx';
 import Quote from './Quote.jsx';
 import Date from './Date.jsx';
+import Profile from './Profile.jsx';
 
 const CV = require('json-loader!yaml-loader!../../CV.yaml');
 console.log(CV);
@@ -13,12 +14,15 @@ class App extends React.Component {
   render () {
     return <div>
       {CV.experiences.map((experience, i) => {
-        if (experience.type === 'work') {
-          return <ExperienceWork experience={experience} key={i} />
-        } else if (experience.type === 'quote') {
-          return <Quote quote={experience} key={i} />
-        } else if (experience.type === 'date') {
-          return <Date date={experience} key={i}/>
+        switch(experience.type) {
+          case 'work':
+            return <ExperienceWork experience={experience} key={i} />;
+          case 'quote':
+            return <Quote quote={experience} key={i} />;
+          case 'date':
+            return <Date date={experience} key={i} />;
+          case 'profile':
+            return <Profile profile={experience} key={i} />;
         }
       })}
     </div>;
