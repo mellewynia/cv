e88b5c86d697408db98c4fed68cb63a6e2780275
Melle Wynia <contact@mellewynia.nl>
2018-09-25 20:51:52 +0200

    Setup 404

---
+++ pages/404.vue
@@ -0,0 +1,32 @@
+<template>
+  <main>
+
+    <div class="content">
+      <h1>Pagina bestaat niet (meer)</h1>
+      <p>Ga naar de <nuxt-link to="/"><u>home pagina</u></nuxt-link></p>
+    </div>
+
+  </main>
+</template>
+
+<script>
+
+export default {
+  components: {
+  },
+  data () {
+    return {
+      date: new Date(...process.env.dateUpdate),
+      timeline: process.env.contents.timeline
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+main {
+  padding: 10vh 20px 16vh;
+  text-align: center;
+  font-size: 16px;
+}
+</style>
